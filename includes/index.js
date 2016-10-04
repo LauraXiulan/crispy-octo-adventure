b4fb7cb43c6b457531d8d6679dc88e6bc35e288e
LauraXiulan <laurakramer91@gmail.com>
2016-10-04 12:12:20 +0200

    Added popup pictures

--- includes/index.js
+++ includes/index.js
@@ -1,35 +1,41 @@
-$('#contact-form').bootstrapValidator({
-//        live: 'disabled',
-        message: 'This value is not valid',
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        fields: {
-            Name: {
-                validators: {
-                    notEmpty: {
-                        message: 'The Name is required and cannot be empty'
-                    }
-                }
-            },
-            email: {
-                validators: {
-                    notEmpty: {
-                        message: 'The email address is required'
-                    },
-                    emailAddress: {
-                        message: 'The email address is not valid'
-                    }
-                }
-            },
-            Message: {
-                validators: {
-                    notEmpty: {
-                        message: 'The Message is required and cannot be empty'
-                    }
-                }
-            }
-        }
-    });
+// Get the modal
+var modal = document.getElementById('myModal');
+var modal1 = document.getElementById('myModal1');
+var modal2 = document.getElementById('myModal2');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var img1 = document.getElementById('myImg1');
+var img2 = document.getElementById('myImg2');
+var modalImg = document.getElementById("img01");
+var modalImg1 = document.getElementById("img02");
+var modalImg2 = document.getElementById("img03");
+var captionText = document.getElementById("caption");
+var captionText1 = document.getElementById("caption1");
+var captionText2 = document.getElementById("caption2");
+
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+img1.onclick = function(){
+    modal1.style.display = "block";
+    modalImg1.src = this.src;
+    captionText1.innerHTML = this.alt;
+}
+
+img2.onclick = function(){
+    modal2.style.display = "block";
+    modalImg2.src = this.src;
+    captionText2.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
